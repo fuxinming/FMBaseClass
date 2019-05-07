fc615a92ac7ed84c86813f058d7c652a9e1a13e5
ios_dev <786186973@qq.com>
2019-05-07 19:38:28 +0800

    提交1个Framework

---
+++ FrameWork/FMBaseClass.framework/Headers/FMBaseClass.h
@@ -0,0 +1,19 @@
+//
+//  FMBaseClass.h
+//  FMBaseClass
+//
+//  Created by FuXinMing on 2019/5/7.
+//  Copyright © 2019 FuXinMing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FMBaseClass.
+FOUNDATION_EXPORT double FMBaseClassVersionNumber;
+
+//! Project version string for FMBaseClass.
+FOUNDATION_EXPORT const unsigned char FMBaseClassVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FMBaseClass/PublicHeader.h>
+
+#import <FMBaseClass/NSString+Validate.h>
---
+++ FrameWork/FMBaseClass.framework/Headers/NSString+Validate.h
@@ -0,0 +1,20 @@
+//
+//  NSString+Validate.h
+//  FMBaseClass
+//
+//  Created by FuXinMing on 2019/5/7.
+//  Copyright © 2019 FuXinMing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Validate)
+
+
+/**
+ *    @brief    URL验证
+ */
+- (BOOL)isURL;
+@end
+
+
